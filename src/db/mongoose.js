fef4916bea5dab82cb3e75a4ddebbc3a1ed52909
Anuj <mailanujhere@gmail.com>
2024-09-09 18:47:36 +0530

    db connection

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,4 +3,16 @@ const mongoose = require('mongoose')
 
 const connectionURL = process.env.CONNECTION_STRING;
 
-mongoose.connect(connectionURL)
+const connectDB = async () => {
+    try{
+        const connect = await mongoose.connect(connectionURL);
+        console.log("Database connected",
+            connect.connection.host,
+            connect.connection.name
+        )
+    }catch (err){
+        console.log("Database not connected", err);
+    }
+};
+
+module.exports = connectDB;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
-require('./db/mongoose')
+const connectDB = require('./db/mongoose')
 const User = require('./models/user')
 const Task = require('./models/task')
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
+
+connectDB();
 
 const app = express()
 const port = process.env.PORT || 3000
